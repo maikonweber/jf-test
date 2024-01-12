8aaac1c61fc05a71b7bfb4273997d083d26b4990
MaikonWeber <maikonweber@gmail.com>
2024-01-12 12:55:14 -0300

    Add professor e aluno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/PrismaModule';
+import { ProfessorModule } from './professor/professor.module';
+import { AlunoModule } from './aluno/aluno.module';
 
 @Module({
   imports: [
-    PrismaModule
+    PrismaModule,
+    ProfessorModule,
+    AlunoModule
   ],
   controllers: [],
   providers: [],
