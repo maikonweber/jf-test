9b1ceefc2a058d6047f9ef8d1d3c31b4894c77f2
MaikonWeber <maikonweber@gmail.com>
2024-01-12 11:12:04 -0300

    Add main swagger and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mutter Corp API')
+    .setDescription('This is Mutter Corporation to access all information')
+    .setVersion('1.0')
+    .addTag('')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
