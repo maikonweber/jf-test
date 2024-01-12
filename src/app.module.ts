8634cb62a5a34870b2d8c4a7e28960517e0f3c1f
MaikonWeber <maikonweber@gmail.com>
2024-01-12 13:16:02 -0300

    Add aula, and curso controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/PrismaModule';
 import { ProfessorModule } from './professor/professor.module';
 import { AlunoModule } from './aluno/aluno.module';
+import { CursoModule } from './curso/curso.module';
+import { AulaModule } from './aula/aula.module';
 
 @Module({
   imports: [
     PrismaModule,
     ProfessorModule,
-    AlunoModule
+    AlunoModule,
+    CursoModule,
+    AulaModule
   ],
   controllers: [],
   providers: [],
