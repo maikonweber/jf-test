f9445a8f7237a74b7924adde537b3dcba74ae870
Maikon Weber <maikonweber@gmail.com>
2024-01-14 22:42:39 -0300

    Add Jest Test

--- src/curso/curso.service.spec.ts
+++ src/curso/curso.service.spec.ts
@@ -1,18 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CursoService } from './curso.service';
+import { PrismaService } from 'prisma/PrismaService';
+import { CreateCursoDto } from './dto/create-curso.dto';
+import { UpdateCursoDto } from './dto/update-curso.dto';
+
+// Mock PrismaService
+const mockPrismaService = {
+  curso: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+    // ... mock other methods ...
+  },
+  curso_aluno: {
+    findMany: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+    findFirst: jest.fn(),
+  },
+  aula_aluno: {
+    findMany: jest.fn(),
+  },
+  // ... mock other services ...
+};
 
 describe('CursoService', () => {
   let service: CursoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CursoService],
+      providers: [
+        CursoService,
+        { provide: PrismaService, useValue: mockPrismaService },
+      ],
     }).compile();
 
     service = module.get<CursoService>(CursoService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
   });
+
+  describe('create', () => {
+    it('should create a new curso', async () => {
+      const createCursoDto: CreateCursoDto = {
+        nome: '',
+        description: '',
+        banner: ''
+      };
+
+      await service.create(createCursoDto);
+
+      expect(mockPrismaService.curso.create).toHaveBeenCalledWith({
+        data: createCursoDto,
+      });
+    });
+  });
+
+
+  describe('findAll', () => {
+    it('should return an array of cursos', async () => {
+      const result = [{ /* provide necessary data for the test */ }];
+
+      mockPrismaService.curso.findMany.mockResolvedValue(result);
+
+      const cursos = await service.findAll();
+
+      expect(cursos).toEqual(result);
+      expect(mockPrismaService.curso.findMany).toHaveBeenCalled();
+    });
+  });
+
 });
